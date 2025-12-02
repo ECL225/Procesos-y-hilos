de67feae74a92a19a53733f64c3c9e7fdce10620
eloicamarasa7 <eloicamarasa7@alumnes.ilerna.com>
2025-12-02 16:00:35 +0100

    Corretgit identation

--- src/UF2/cFilPrincipal.java
+++ src/UF2/cFilPrincipal.java
@@ -1,31 +1,29 @@
 package UF2;
 
 public class cFilPrincipal {
+    //MAIN BRANCH
+    public static void main (String [] pArguments) {
+        System.out.println ("Fil principal iniciat.");
+        System.out.println ("Fil secundari iniciat.");
 
-  public static void main (String [] pArguments) {
+        cFil vObjecteFil = new cFil ("#1");
+        //alternativa: innecessari
+        Thread vFil = new Thread (vObjecteFil);
 
-    System.out.println ("Fil principal iniciat.");
-    System.out.println ("Fil secundari iniciat.");
+        //alternativa: vObjecteFil
+        vFil.start ();
+        System.out.println ("Iniciant execució procés principal");
 
-    cFil vObjecteFil = new cFil ("#1");
-
-    //alternativa: innecessari
-    Thread vFil = new Thread (vObjecteFil);
-
-    //alternativa: vObjecteFil
-    vFil.start ();
-    System.out.println ("Iniciant execució procés principal");
-
-    try {
-      for (int vComptador = 0; vComptador < 10; vComptador ++) {
-        Thread.sleep(500);
-        System.out.println("Despertant aturada " + vComptador + " procès principal");
-      }
-    }
-    catch (InterruptedException pExcepcio) {
-      System.out.println ("Interrompent execució procès principal");
-    }
-    System.out.println ("Acabant execució procès principal");
+        try {
+            for (int vComptador = 0; vComptador < 10; vComptador ++) {
+                Thread.sleep(500);
+                System.out.println("Despertant aturada " + vComptador + " procès principal");
+            }
+        }
+        catch (InterruptedException pExcepcio) {
+            System.out.println ("Interrompent execució procès principal");
+        }
+        System.out.println ("Acabant execució procès principal");
   }
 
 }
