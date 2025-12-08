76f71b5d7087bfd074f996493787831df2e6b9d0
ECL225 <eloicamarasa7@alumnes.ilerna.com>
2025-12-08 16:43:26 +0100

    Exercici 1-4

--- src/UF2/cFilPrincipal.java
+++ src/UF2/cFilPrincipal.java
@@ -1,20 +1,34 @@
 package UF2;
 
+import java.util.Scanner;
+
 public class cFilPrincipal {
-    //MAIN BRANCH
+    //EXERCICI 1.4
     public static void main (String [] pArguments) {
+        Scanner scanN = new Scanner(System.in);
+        System.out.println("Introdueix quants fils vols activar");
+        int cant = scanN.nextInt();
         System.out.println ("Fil principal iniciat.");
         System.out.println ("Fil secundari iniciat.");
-
-        cFil vObjecteFil = new cFil ("#1");
-        //alternativa: innecessari
-        Thread vFil = new Thread (vObjecteFil);
-
-        //alternativa: vObjecteFil
-        vFil.start ();
-        System.out.println ("Iniciant execució procés principal");
-
+        int t = 100;
+        cFil vObjecteFilFinal = new cFil ("Fill child FINAL");
+        Thread vFilF = new Thread (vObjecteFilFinal);
+        for (int i = 1; i <=cant; i++) {
+            if (i!=cant){
+                cFil vObjecteFil = new cFil ("Fill child Nº:"+Integer.toString(i));
+                vObjecteFil.sTemporitzacio(t);
+                t=t+100;
+                Thread vFil = new Thread (vObjecteFil);
+                vFil.start();
+            }
+            else {
+                vObjecteFilFinal.sTemporitzacio(t);
+                vFilF.start();
+            }
+        }
         try {
+            System.out.println ("Iniciant execució procés principal");
+            vFilF.join();
             for (int vComptador = 0; vComptador < 10; vComptador ++) {
                 Thread.sleep(500);
                 System.out.println("Despertant aturada " + vComptador + " procès principal");
