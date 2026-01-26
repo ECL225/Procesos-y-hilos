e0bdf15cb0dcd965436d7c0705f08474090f09f9
alex24050714 <alexmontserrat2405a@alumnes.ilerna.com>
2026-01-26 13:03:23 +0100

    Inici E2-1

--- src/UF2/cFilPrincipal.java
+++ src/UF2/cFilPrincipal.java
@@ -1,9 +1,26 @@
 package UF2;
 
+import java.util.Scanner;
+
 public class cFilPrincipal {
-    //MAIN BRANCH
+    //E2-1 Calcul concurrent sumes parcials
     public static void main (String [] pArguments) {
-        System.out.println ("Fil principal iniciat.");
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Introdueix el numero de fils que vols crear: ");
+        int numFils = scan.nextInt();
+
+        int[] valors = new int[numFils];
+        for (int i = 0; i < numFils; i++){
+            System.out.println("Introdueix en N per el fil " + i);
+            valors[i] = scan.nextInt();
+        }
+
+        Thread[] fils = new Thread[numFils];
+
+
+
+        /*System.out.println ("Fil principal iniciat.");
         System.out.println ("Fil secundari iniciat.");
 
         cFil vObjecteFil = new cFil ("#1");
@@ -23,7 +40,7 @@ public class cFilPrincipal {
         catch (InterruptedException pExcepcio) {
             System.out.println ("Interrompent execució procès principal");
         }
-        System.out.println ("Acabant execució procès principal");
+        System.out.println ("Acabant execució procès principal");*/
   }
 
 }
