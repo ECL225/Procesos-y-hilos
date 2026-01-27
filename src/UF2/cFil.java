a0ea6c4ee28894747219bae32d59538b196b15b7
alex24050714 <alexmontserrat2405a@alumnes.ilerna.com>
2026-01-27 12:41:37 +0100

    Comentaris i ajustament de codi

--- src/UF2/cFil.java
+++ src/UF2/cFil.java
@@ -1,36 +1,41 @@
 package UF2;
 
 public class cFil extends Thread {
-    private int numFils;
-    private long resultat;
+    private int numFils; // l'index del fil per calcular el fibonacci
+    private long resultat; // on es guarda el resultat del fibonacci
 
+    //Constructor que rep el numero de fils
     public cFil (int numFils) {
         this.numFils = numFils;
     }
 
+    //Mètode que calcula el fibonacci de la posició que es dona
     private long fibonacci(int posicio){
+        //Si la posicio donada es 0 o 1 (casos inicials) retorna directament el mateix numero
         if (posicio <= 1){
             return posicio;
         }
-        long num1 = 0;
-        long num2 = 1;
 
+        //Els dos primers numeros de la serie fibonacci
+        long num1 = 0; // anterior comença amb 0
+        long num2 = 1; //actual comença amb 1
+
+        //Bucle per calcular fibonacci segons la posicio o numero de fil
         for (int i = 2; i <= posicio; i++){
-            long resultat = num1 + num2;
-            num1 = num2;
-            num2 = resultat;
+            long resultat = num1 + num2; //cada numero es igual a la suma dels 2 anteriors
+            num1 = num2; // el anterior ara es l'actual
+            num2 = resultat; // el nou es el resultat de la suma
         }
-        return num2;
+        return num2; // l'actual conte el resultat final del fibonacci
     }
+
+    //Quan s'executa el fil es calcula el fibonacci i el guarda a resultat
     @Override
     public void run () {
         resultat = fibonacci(numFils);
     }
+    //Getter per poder llegir el resultat
     public long getResultat(){
         return resultat;
-    }
-
-    public int getNumFils(){
-        return numFils;
     }
 }
--- src/UF2/cFilPrincipal.java
+++ src/UF2/cFilPrincipal.java
@@ -7,22 +7,27 @@ public class cFilPrincipal {
     public static void main (String [] pArguments) {
         Scanner scan = new Scanner(System.in);
 
+        //Demanem a l'usuari el numero de fils que vol
         System.out.println("Introdueix el numero de fils que vols crear: ");
         int numFils = scan.nextInt();
 
+        //Un cop l'usuari introdueix el numero de fils que vol els passem a guardar a un array de fils i els iniciem
         cFil[] fil = new cFil[numFils];
         for (int i = 0; i < numFils; i++){
-            fil[i]=new cFil(i);
-            fil[i].start();
+            fil[i]=new cFil(i); // fil i calcularà el fibonacci que correspongui a la i
+            fil[i].start(); // s'inicia el fil
         }
+
+        //Amb el següent for esperem que els fils acabin abans de continuar gracies al join
         for (int i = 0; i < numFils; i++){
             try {
-                fil[i].join();
-            }catch (InterruptedException e){
+                fil[i].join(); // espera al fil i especific
+            }catch (InterruptedException e){ // si s'interrom el fil mentres esperen es mostre un error
                 e.printStackTrace();
             }
         }
 
+        //El següent for imprimeix en ordre creixent del numFils (index)
         for (int i = 0; i < numFils; i++){
             System.out.println("Fil numero: (" + i + ") fibonacci = " + fil[i].getResultat());
         }
