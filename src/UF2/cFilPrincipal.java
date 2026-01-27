bcdc798924de91244afe402075f932bed61bed39
eloicamarasa7 <eloicamarasa7@alumnes.ilerna.com>
2026-01-27 16:20:06 +0100

    2-5 exercici complert

--- src/UF2/cFilPrincipal.java
+++ src/UF2/cFilPrincipal.java
@@ -5,41 +5,41 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class cFilPrincipal {
-    //E2-3 BÚSQUEDA DEL VALOR MÀXIM D’UN VECTOR
+    //E2-5 COMPROBACIÓ DE NÚMEROS PRIMERS
 
     public static void main (String [] pArguments) {
         Scanner scanN = new Scanner(System.in);
-
-        System.out.println("Indica el tamany del vector:");
-        int tVector = scanN.nextInt();
         int nEntrat = -2;
 
-        ArrayList<Integer> vectorArray = new ArrayList<Integer>(); // Arraylist amb els numeros entrats per pantalla
-        for (int i = 0; i < tVector; i++) {
-            vectorArray.add(rand.nextInt(10000));
-        }
-        System.out.println("Introdueix numeros, quan et cansis introdueix -1: ");
+        System.out.println("\n\n\n\n Introdueix numeros, quan et cansis introdueix -1: ");
         ArrayList<Integer> numerosEntrats = new ArrayList<>();
-        while (nEntrat != -1 || nEntrat == -2) {// Input de usuari
-            System.out.println("Indica el numero de fils(Ha de ser menor o igual al Tamany del vector):");
+        while (nEntrat != -1 || nEntrat == -2) {// Input de usuari de numeros
             nEntrat = scanN.nextInt();
-            numerosEntrats.add(nEntrat);
+            if (!(nEntrat == -1 || nEntrat == -2)){// no entrar aquests numeros ja que son els que pane el bucle
+                numerosEntrats.add(nEntrat);
+            }
         }
-        //System.out.println("Resultat Divisio: "+divResult);
         ArrayList<cFil> ArrayFils = new ArrayList<>();// Arraylist dels fils a executar
         for (int i = 0; i < numerosEntrats.size(); i++) {//Creacio e insercio dels fils a executar al arraylist
             cFil fNumeroPrimo = new cFil(numerosEntrats.get(i));
             ArrayFils.add(fNumeroPrimo);
         }
-        for (int i = 0; i < ArrayFils.size(); i++) {
-            ArrayFils.get(i).join();
-            if (ArrayFils.get(i).getnMGran()> nMGran){
-                nMGran = ArrayFils.get(i).getnMGran();
-            }
+        for (int i = 0; i < ArrayFils.size(); i++) {// inicia els fils
+            ArrayFils.get(i).start();
         }
-
-
-        System.out.println("Nº mes gran dins del vector: "+ nMGran);
+        try {
+            for (int i = 0; i < ArrayFils.size(); i++) {//comprobar que es primo amb la funcio isPrime de cada fil i mostrar la resposta
+                ArrayFils.get(i).join();
+                if (ArrayFils.get(i).isPrime){
+                    System.out.println("El numero "+ numerosEntrats.get(i)+" en la posicion " + (i+1) + " es primo.");
+                }
+                else {
+                    System.out.println("El numero "+ numerosEntrats.get(i)+" en la posicion " + (i+1) + " NO es primo.");
+                }
+            }
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 
