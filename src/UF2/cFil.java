e9e8f30a01c775431fe0cbe14c25cc35837a93c8
alex24050714 <alexmontserrat2405a@alumnes.ilerna.com>
2026-01-27 13:05:27 +0100

    E2-1 comentat

--- src/UF2/cFil.java
+++ src/UF2/cFil.java
@@ -1,31 +1,34 @@
 package UF2;
 
-import java.util.Scanner;
-
-//alternativa: extends Thread
 public class cFil extends Thread {
-    private int numFils;
-    private int resultat;
-    private int N;
+    private int numFils; // identificador del fil
+    private int resultat; // on es guarda la suma calculada
+    private int N; // valor màxim per calcular la suma
 
-  public cFil (int numFils, int N) {
-      this.numFils = numFils;
-      this.N = N;
-  }
+    //Constructor que rep l'identificador del fil i el seu N
+    public cFil (int numFils, int N) {
+        this.numFils = numFils;
+        this.N = N;
+    }
 
-  @Override
-  public void run () {
-      resultat = 0;
+    //Quan s'executa el fil, calcula la suma 1+2+...+N
+    @Override
+    public void run () {
+        resultat = 0; // s'inicialitza el resultat a 0
 
-      for (int i = 1; i <= N; i++){
-          resultat += i;
-      }
-  }
+        //Bucle per sumar tots els numeros de 1 fins N
+        for (int i = 1; i <= N; i++){
+            resultat += i; // es va sumant i al resultat acumulat
+        }
+    }
+
+    //Getter perque el main pugui llegir el resultat de la suma
     public int getResultat(){
         return resultat;
     }
 
+    //Getter per identificar quin fil es
     public int getNumFils(){
-      return numFils;
+        return numFils;
     }
 }
--- src/UF2/cFilPrincipal.java
+++ src/UF2/cFilPrincipal.java
@@ -7,32 +7,38 @@ public class cFilPrincipal {
     public static void main (String [] pArguments) {
         Scanner scan = new Scanner(System.in);
 
+        //Demanem a l'usuari el número de fils que vol crear
         System.out.println("Introdueix el numero de fils que vols crear: ");
         int numFils = scan.nextInt();
 
+        //Demanem a l'usuari els valors N per cada fil
         int[] valors = new int[numFils];
         for (int i = 0; i < numFils; i++){
             System.out.println("Introdueix en N per el fil " + (i+1));
-            valors[i] = scan.nextInt();
+            valors[i] = scan.nextInt(); // guardem N per al fil i
         }
 
+        //Creem arrays per guardar els objectes cFil i els Threads
         Thread[] fils = new Thread[numFils];
         cFil[] filsObjectes = new cFil[numFils];
 
+        //Creem i iniciem tots els fils
         for (int i = 0; i < numFils; i++){
-            filsObjectes[i] = new cFil(i +1, valors[i]);
-            fils[i] = new Thread(filsObjectes[i]);
-            fils[i].start();
+            filsObjectes[i] = new cFil(i +1, valors[i]); // objecte amb id i N
+            fils[i] = new Thread(filsObjectes[i]); // es crea el fil amb el cFil
+            fils[i].start(); // s'executa el run() del cFil
         }
 
+        //Amb el següent for esperem que els fils acabin abans de continuar gracies al join
         for (int i = 0; i < numFils; i++){
             try{
-                fils[i].join();
-            }catch (InterruptedException e){
+                fils[i].join(); // espera al fil i especific
+            }catch (InterruptedException e){ // si s'interrom el fil mentres esperen es mostre un error
                 e.printStackTrace();
             }
         }
 
+        //S'imprimeix els resultats en ordre del numero de fil
         for(int i = 0; i < numFils; i++) {
             System.out.println("Fil: " + filsObjectes[i].getNumFils() + " Resultat: " + filsObjectes[i].getResultat());
         }
